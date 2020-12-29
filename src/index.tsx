964ba67043d1f4ba6d353c67e808ab5afc14321d
Tim Keresey <tim.keresey@gmail.com>
2020-12-29 11:33:10 -0700

    remove example components

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Rendering a Functional Component
-const FunctionalComponent = () => <div>1) I am a stateless React TypeScript functional component</div>;
-
-// Rendering Functional components with props
-interface Props {
-    name: string,
-}
-const FunctionalComponentWithProps: React.FC<Props> = (props) => {
-    return <div>{props.name}</div>;
-}
+// // Rendering a Functional Component
+// const FunctionalComponent = () => <div>1) I am a stateless React TypeScript functional component</div>;
+//
+// // Rendering Functional components with props
+// interface Props {
+//     name: string,
+// }
+// const FunctionalComponentWithProps: React.FC<Props> = (props) => {
+//     return <div>{props.name}</div>;
+// }
 
 ReactDOM.render(
   <React.StrictMode>
-    <FunctionalComponent />
-    <FunctionalComponentWithProps name={'2) I am a React TypeScript functional component with props'} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
