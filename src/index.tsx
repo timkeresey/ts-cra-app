df1cf7a922d6128f9b1b1ac2a917c53989702369
Tim Keresey <tim.keresey@gmail.com>
2020-12-29 11:30:17 -0700

    render functional component with props

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Rendering a Functional Component
-const FunctionalComponent = () => {
-  return (
-    <div>
-    1- I am a stateless React TypeScript functional component</div>
-  )
-};
+const FunctionalComponent = () => <div>1) I am a stateless React TypeScript functional component</div>;
+
+// Rendering Functional components with props
+interface Props {
+    name: string,
+}
+const FunctionalComponentWithProps: React.FC<Props> = (props) => {
+    return <div>{props.name}</div>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <FunctionalComponent />
+    <FunctionalComponentWithProps name={'2) I am a React TypeScript functional component with props'} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
